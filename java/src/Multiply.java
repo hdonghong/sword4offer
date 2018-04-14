a837fc53edd739b1b03eccbe156876590191695f
hdonghong <799108252@qq.com>
2018-04-14 23:29:18 +0800

    16th

---
+++ java/src/Multiply.java
@@ -0,0 +1,19 @@
+/**
+ * Multiply class<br/>
+ *
+ * 构建乘积数组 <br/>
+ *
+ * 题目描述<br/>
+ * 给定一个数组A[0,1,...,n-1],请构建一个数组B[0,1,...,n-1],
+ * 其中B中的元素B[i]=A[0]*A[1]*...*A[i-1]*A[i+1]*...*A[n-1]。不能使用除法。<br/>
+ *
+ * @author hdonghong
+ * @date 2018/04/14
+ */
+public class Multiply {
+
+    public int[] multiply(int[] A) {
+        int[] B = new int[A.length];
+        return B;
+    }
+}
