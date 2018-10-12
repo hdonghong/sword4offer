6560e360e80ddb6fc798f81856d56ad932ef3581
hdonghong <799108252@qq.com>
2018-10-12 23:26:14 +0800

    reach 40

--- java/src/Convert.java
+++ java/src/Convert.java
@@ -1,11 +1,12 @@
+import java.util.Stack;
+
 /**
  * Convert class<br/>
- *
+ * 二叉搜索树与双向链表
  * 题目描述
  * 一棵二叉搜索树，将该二叉搜索树转换成一个排序的双向链表。要求不能创建任何新的结点，只能调整树中结点指针的指向。
- * TODO
  * @author hdonghong
- * @date 2018/05/26
+ * @since 2018/05/26
  */
 public class Convert {
 
@@ -22,7 +23,32 @@ public class Convert {
      */
 
     public TreeNode Convert(TreeNode pRootOfTree) {
-        return null;
+        TreeNode[] lastNodeInList = new TreeNode[1];
+        convertNode(pRootOfTree, lastNodeInList);
+        // 返回头节点
+        TreeNode headInList = lastNodeInList[0];
+        while (headInList != null && headInList.left != null) {
+            headInList = headInList.left;
+        }
 
+        return headInList;
+
+    }
+
+    private void convertNode(TreeNode current, TreeNode[] lastNodeInList) {
+        if (current == null) return;
+
+        // 先递归左节点
+        if (current.left != null) convertNode(current.left, lastNodeInList);
+
+        // 互指向
+        current.left = lastNodeInList[0];
+        if (lastNodeInList[0] != null) lastNodeInList[0].right = current;
+
+        // 更新指向列表中最后节点
+        lastNodeInList[0] = current;
+
+        // 最后递归右节点
+        if (current.right != null) convertNode(current.right, lastNodeInList);
     }
 }
---
+++ java/src/IsSymmetrical.java
@@ -0,0 +1,40 @@
+/**
+ * IsSymmetrical class<br/>
+ *
+ * 对称的二叉树
+ * 题目描述
+ * 请实现一个函数，用来判断一颗二叉树是不是对称的。
+ * 注意，如果一个二叉树同此二叉树的镜像是同样的，定义其为对称的。
+ * TODO
+ * @author hdonghong
+ * @since 2018/10/12
+ */
+public class IsSymmetrical {
+
+    /*
+        public class TreeNode {
+            int val = 0;
+            TreeNode left = null;
+            TreeNode right = null;
+
+            public TreeNode(int val) {
+                this.val = val;
+
+            }
+        }
+    */
+
+    boolean isSymmetrical(TreeNode pRoot) {
+        return isSymmetrical(pRoot, pRoot);
+    }
+
+    boolean isSymmetrical(TreeNode node, TreeNode node2) {
+        if (node == null && node2 == null) return true;
+
+        if (node == null || node2 == null) return false;
+
+        if (node.val != node2.val) return false;
+
+        return isSymmetrical(node.left, node2.right) && isSymmetrical(node.right, node2.left);
+    }
+}
